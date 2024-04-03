66723eb4c149f1c67ca0d8c7df7c0b6ece3e792a
1989ONCE <susanloveselena@gmail.com>
2024-04-03 22:32:31 +0800

    refactor: change CompositionFigure to GroupFigure

--- src/canvas/btnAction/CreateGroupAction.java
+++ src/canvas/btnAction/CreateGroupAction.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import canvas.MyCanvas;
-import canvas.shape.CompositeFigure;
+import canvas.shape.GroupFigure;
 import canvas.shape.Figure;
 import init.MyFrame;
 
@@ -18,7 +18,7 @@ public class CreateGroupAction extends MouseAdapter implements ButtonAction{
     @Override
     public void mousePressed(MouseEvent e) {
         this.startPoint = e.getPoint();
-        tempGroupFigure = new CompositeFigure(0, 0, 0, 0);
+        tempGroupFigure = new GroupFigure(0, 0, 0, 0);
     }
 
     @Override
@@ -29,14 +29,14 @@ public class CreateGroupAction extends MouseAdapter implements ButtonAction{
         int height = endPoint.y - startPoint.y;
 
         // Paint the GroupFigure only if there are more than 1 basicfigure(composite figure not allowed) inside the GroupFigure
-        if (((CompositeFigure)tempGroupFigure).getFigures().size() > 1) {
+        if (((GroupFigure)tempGroupFigure).getInsideFigures().size() > 1) {
             tempGroupFigure.updatePorts(startPoint.x, startPoint.y, width, height);
             tempGroupFigure.setFigureName("Group");
             canvas.addFigure(tempGroupFigure);
             canvas.clearAllSelected();
             tempGroupFigure.setSelected(tempGroupFigure, true);
             canvas.setSelectedFigure(tempGroupFigure);
-            for (Figure figure : ((CompositeFigure)tempGroupFigure).getFigures()) {
+            for (Figure figure : ((GroupFigure)tempGroupFigure).getInsideFigures()) {
                 figure.setParent(tempGroupFigure);
                 figure.setPortVisibility(true);
             }
@@ -59,7 +59,7 @@ public class CreateGroupAction extends MouseAdapter implements ButtonAction{
         int width = endPoint.x - startPoint.x;
         int height = endPoint.y - startPoint.y;
         // Create a temporary group figure to show the process of creating a group figure
-        tempGroupFigure = new CompositeFigure(startPoint.x, startPoint.y, width, height);
+        tempGroupFigure = new GroupFigure(startPoint.x, startPoint.y, width, height);
         canvas.setTempFigure(tempGroupFigure);
 
         // Check if the figures are inside the tempGroupFigure
@@ -67,7 +67,7 @@ public class CreateGroupAction extends MouseAdapter implements ButtonAction{
         for (Figure figure : allFigures) {
             if(figure.getParent() != null) continue; // Skip the figure that is already in a group
             else if (figure.inSide(figure, startPoint.x, startPoint.y, width, height)) {
-                ((CompositeFigure)tempGroupFigure).addFigureToList(figure);
+                ((GroupFigure)tempGroupFigure).addFigureToList(figure);
             }
             
         }
--- src/canvas/shape/GroupFigure.java
+++ src/canvas/shape/GroupFigure.java
@@ -4,8 +4,8 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import canvas.btnAction.GroupInterface;
 
-public class CompositeFigure extends Figure implements GroupInterface {
-    public CompositeFigure(int x, int y, int width, int height) {
+public class GroupFigure extends Figure implements GroupInterface {
+    public GroupFigure(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
     private ArrayList<Figure> compositeFigures = new ArrayList<>();
@@ -28,56 +28,22 @@ public class CompositeFigure extends Figure implements GroupInterface {
             figure.move(dx, dy);
         }
     }
-    @Override
-    public void resizeBasedOn(Port port) {
-        for (Figure figure : compositeFigures) {
-            figure.resizeBasedOn(port);
-        }
-    }
+    // @Override
+    // public void resizeBasedOn(Port port) {
+    //     for (Figure figure : compositeFigures) {
+    //         figure.resizeBasedOn(port);
+    //     }
+    // }
 
     @Override
     public void addFigureToList(Figure figure) {
         this.compositeFigures.add(figure);
     }
-
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
+    
     // Add getters and setters for x, y, width, height here...
     // Add methods for handling ports here...
-    public void setPortSize(int size) {
-        topPort.setSize(size);
-        rightPort.setSize(size);
-        bottomPort.setSize(size);
-        leftPort.setSize(size);
-    }
 
-    public ArrayList<Figure> getFigures() {
+    public ArrayList<Figure> getInsideFigures() {
         return compositeFigures;
     }
 }
