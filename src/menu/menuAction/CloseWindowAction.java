784e58c7dc323cbfc3e0977343eb9d39f46c253e
1989ONCE <susanloveselena@gmail.com>
2024-04-04 17:37:44 +0800

    fix: closing the window but the program still runs

--- src/menu/menuAction/CloseWindowAction.java
+++ src/menu/menuAction/CloseWindowAction.java
@@ -7,9 +7,13 @@ public class CloseWindowAction implements MenuAction {
     public void execute() {
         MyFrame frame = MyFrame.getFrame();
         MyCanvas canvas = frame.getCanvas();
+
+        // keepOpen would be false if user click "Don't Save" in the confirm dialog
         boolean keepOpen = canvas.showConfirmDialog(canvas);
         if (!keepOpen){
+            // close the window and exit the program
             frame.dispose();
+            System.exit(0);
         }
     }
 }
