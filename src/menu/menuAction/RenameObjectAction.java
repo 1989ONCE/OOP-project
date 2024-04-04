9c7954d3fce2ebe9fd40a115a6db825aca1eb625
1989ONCE <susanloveselena@gmail.com>
2024-04-04 14:42:21 +0800

    fix: exclude some situation for rename feature

--- src/menu/menuAction/RenameObjectAction.java
+++ src/menu/menuAction/RenameObjectAction.java
@@ -2,7 +2,9 @@ package menu.menuAction;
 
 import javax.swing.JOptionPane;
 
+import canvas.MyCanvas;
 import canvas.shape.Figure;
+import canvas.shape.GroupFigure;
 import init.MyFrame;
 
 /* UseCase F.1 Change Object Name 
@@ -12,10 +14,17 @@ import init.MyFrame;
 public class RenameObjectAction implements MenuAction{
     @Override
     public void execute() {
+        MyCanvas canvas = MyFrame.getFrame().getCanvas();
         Figure selectedFigure = MyFrame.getFrame().getCanvas().getSelectedFigure();
-        
-        // if no figure is selected, do nothing and return
-        if(selectedFigure == null) return;
+        Figure tempGroupFigure = MyFrame.getFrame().getCanvas().getTempFigure();
+        // if no figure is selected or there is a tempGroupFigure, do nothing and return
+        if(selectedFigure == null || tempGroupFigure != null){
+            canvas.clearAllSelected();
+            ((GroupFigure)tempGroupFigure).getInsideFigures().clear();
+            canvas.setTempFigure(null);
+            canvas.repaint();
+            return;
+        }
         
         String newName = JOptionPane.showInputDialog("Enter the new name of the object", selectedFigure.getFigureName());
         // if the user clicks cancel, newName will be null
